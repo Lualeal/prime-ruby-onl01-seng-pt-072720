44a0c716def77a5a8bd2d66973bc6e419f4b399b
Luana Leal <luana.leal.eq@gmail.com>
2020-08-06 05:04:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(integer)
+  if integer < 2
+    return false
+  end
+  
+  number = 2 
+  
+  while number < integer
+    if integer % number == 0
+     return false 
+    end
+    number += 1
+  end
+  return true
+end 
+    
